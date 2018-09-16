cec6b325b976f199724863a9a7dfeb20ec0f84ce
Pranay Kumar Rasulury <pranay.rasulury1@gmail.com>
2018-09-16 15:56:47 -0500

    new update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ma-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/phones/phone.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ma-phone',
+  templateUrl: './phone.component.html'
+})
+
+export class PhoneComponent{
+}
